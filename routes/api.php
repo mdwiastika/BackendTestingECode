fd6297f223d4797cacfb034c1e298379b5f4b109
Marcel Dwi Astika <marceldwias@gmail.com>
2024-01-15 06:16:38 +0700

    add:Get User Login

--- routes/api.php
+++ routes/api.php
@@ -24,7 +24,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user-login', function (Request $request) {
     return $request->user();
 });
 
